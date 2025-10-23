0204e4f362096f9dff29851aad9849dac14ff79a
Alexander Klee <alexander.m.klee@gmail.com>
2025-10-23 13:25:13 +0200

    feat: improve control flow

--- main.cpp
+++ main.cpp
@@ -22,110 +22,89 @@ class VideoDecoder {
     AVPacket* packet;
     AVFrame* frame;
 
-    public:
-        explicit VideoDecoder(const std::string &filename) {
-            _filename = filename;
-            // create Context for a video
-            format_context = avformat_alloc_context();
-            avformat_open_input(&format_context, filename.c_str(), nullptr, nullptr); // populate
-            avformat_find_stream_info(format_context, nullptr);
+public:
+    explicit VideoDecoder(const std::string &filename) { // TODO: better error handling
+        _filename = filename;
+        // create Context for a video
+        format_context = avformat_alloc_context();
+        avformat_open_input(&format_context, filename.c_str(), nullptr, nullptr); // populate
+        avformat_find_stream_info(format_context, nullptr);
 
-            // print info, duration seems wrong?
-            // std::cout << "Format: " << format_context->iformat->name << ", duration: " << format_context->duration / AV_TIME_BASE << "s" << std::endl;
+        // print info, duration seems wrong?
+        // std::cout << "Format: " << format_context->iformat->name << ", duration: " << format_context->duration / AV_TIME_BASE << "s" << std::endl;
 
-            // find video stream
-            video_stream_index = av_find_best_stream(format_context, AVMEDIA_TYPE_VIDEO, -1, -1, nullptr, 0);
-            video_stream = format_context->streams[video_stream_index];
+        // find video stream
+        video_stream_index = av_find_best_stream(format_context, AVMEDIA_TYPE_VIDEO, -1, -1, nullptr, 0);
+        video_stream = format_context->streams[video_stream_index];
 
-            // create decoder (and context) for video stream
-            decoder = avcodec_find_decoder(format_context->streams[video_stream_index]->codecpar->codec_id);
-            decoder_context = avcodec_alloc_context3(decoder);
-            avcodec_parameters_to_context(decoder_context, video_stream->codecpar);
-            avcodec_open2(decoder_context, decoder, nullptr);
+        // create decoder (and context) for video stream
+        decoder = avcodec_find_decoder(format_context->streams[video_stream_index]->codecpar->codec_id);
+        decoder_context = avcodec_alloc_context3(decoder);
+        avcodec_parameters_to_context(decoder_context, video_stream->codecpar);
+        avcodec_open2(decoder_context, decoder, nullptr);
 
-            packet = av_packet_alloc();
-            frame = av_frame_alloc();
+        packet = av_packet_alloc();
+        frame = av_frame_alloc();
+    }
+
+    ~VideoDecoder() {
+        av_packet_free(&packet);
+        av_frame_free(&frame);
+        avformat_close_input(&format_context);
+    }
+
+    void dump_info() const {
+        // print info on stderr
+        av_dump_format(format_context, 0, _filename.c_str(), 0);
+    }
+
+    [[nodiscard]] int get_width() const {
+        return format_context->streams[video_stream_index]->codecpar->width;
+    }
+
+    [[nodiscard]] int get_height() const {
+        return format_context->streams[video_stream_index]->codecpar->height;
+    }
+
+    [[nodiscard]] int get_pixel_format() const {
+        return format_context->streams[video_stream_index]->codecpar->format;
+    }
+
+    [[nodiscard]] std::expected<std::vector<uint8_t>, std::string> next_frame() const {
+        // read frames from streams in the file until EOF
+        if (av_read_frame(format_context, packet) < 0) {
+            return std::unexpected("failed to read frame or EOF");
         }
 
-        ~VideoDecoder() {
-            av_packet_free(&packet);
-            av_frame_free(&frame);
-            avformat_close_input(&format_context);
+        // check if stream is the chosen stream
+        if (packet->stream_index != video_stream_index) {
+            return std::unexpected("packet is not part of video stream");
         }
 
-        void dump_info() const {
-            // print info on stderr
-            av_dump_format(format_context, 0, _filename.c_str(), 0);
+        // video has only one frame per packet
+        // decode packet to get frame
+        if (avcodec_send_packet(decoder_context, packet) < 0) {
+            return std::unexpected("failed to send packet");
+        }
+        if (avcodec_receive_frame(decoder_context, frame) < 0) {
+            return std::unexpected("failed to receive frame");
         }
 
-        [[nodiscard]] int get_width() const {
-            return format_context->streams[video_stream_index]->codecpar->width;
+        auto pixel_format = static_cast<enum AVPixelFormat>(frame->format);
+        int buf_size = av_image_get_buffer_size(pixel_format, get_width(), get_height(), 1);
+        std::vector<uint8_t> image_buf(buf_size);
+
+        // copy the image data
+        if (av_image_copy_to_buffer(image_buf.data(), buf_size,
+                frame->data, frame->linesize, pixel_format, get_width(), get_height(), 1) < 0) {
+            return std::unexpected("failed to copy frame");
         }
 
-        [[nodiscard]] int get_height() const {
-            return format_context->streams[video_stream_index]->codecpar->height;
-        }
-
-        [[nodiscard]] int get_pixel_format() const {
-            return format_context->streams[video_stream_index]->codecpar->format;
-        }
-
-        std::expected<std::vector<uint8_t>, std::string> next_frame() const {
-            // read frames from streams in the file until EOF
-            if (av_read_frame(format_context, packet) >= 0) {
-                // check if stream is the chosen stream
-                if (packet->stream_index == video_stream_index) {
-                    // video has only one frame per packet
-
-                    // decode packet to get frame
-                    int ret = avcodec_send_packet(decoder_context, packet);
-                    if (ret < 0) {
-                        return std::unexpected("failed to send packet");
-                    }
-
-                    ret = avcodec_receive_frame(decoder_context, frame);
-                    if (ret < 0) {
-                        return std::unexpected("failed to recieve frame");
-                        // switch (ret) {
-                        //     case AVERROR(EAGAIN):
-                        //         std::cerr << "AVERROR(EAGAIN)" << std::endl;
-                        //         break;
-                        //     case AVERROR_EOF:
-                        //         std::cerr << "AVERROR_EOF" << std::endl;
-                        //         break;
-                        //     case AVERROR(EINVAL):
-                        //         std::cerr << "EINVAL" << std::endl;
-                        //         break;
-                        //     default:
-                        //         std::cerr << "Unknown error" << std::endl;
-                        // }
-                    }
-
-
-                    std::cout << "width: " << get_width() << " height: " << get_height() << " frame num: " << decoder_context->frame_num << std::endl;
-
-                    auto pixel_format = static_cast<enum AVPixelFormat>(frame->format);
-                    int buf_size = av_image_get_buffer_size(pixel_format, get_width(), get_height(), 1);
-                    std::vector<uint8_t> buf(buf_size);
-
-                    ret = av_image_copy_to_buffer(buf.data(), buf_size,
-                        frame->data, frame->linesize,
-                        pixel_format, get_width(), get_height(), 1);
-
-                    if (ret < 0) {
-                        return std::unexpected("failed to copy frame");
-                    }
-
-                    // wipe frame, packet for next iteration
-                    av_frame_unref(frame);
-                    av_packet_unref(packet);
-                    return buf;
-                }
-
-            } else {
-                return std::unexpected("EOF");
-            }
-        }
+        // wipe frame and packet for next iteration
+        av_frame_unref(frame);
+        av_packet_unref(packet);
+        return image_buf;
+    }
 };
 
 void save_pgm(const std::vector<uint8_t>& data, int width, int height, const std::string& filename) {
@@ -177,12 +156,5 @@ int main(int argc, char* argv[]) {
             std::cout << res.error() << std::endl;
         }
     }
-
-    // auto start = std::chrono::steady_clock::now();
-
-    // auto end = std::chrono::steady_clock::now();
-    // auto elapsed = std::chrono::duration_cast<std::chrono::seconds>(end - start);
-    // std::cout << std::endl << "time elapsed: " << elapsed.count() << "s, "
-    //     << video_frame_count/elapsed.count() << " fps"<< std::endl;
     return 0;
 }
